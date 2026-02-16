1735f03b391fa39fef2e274e1e3ab69fb3b2da06
HTRMC <143692436+HTRMC@users.noreply.github.com>
2026-02-16 13:02:00 +0100

    feat: integrate window and renderer in main loop

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,32 @@
 const std = @import("std");
-const Io = std.Io;
-
-const FarHorizons = @import("FarHorizons");
+const Window = @import("platform/Window.zig").Window;
+const Renderer = @import("renderer/Renderer.zig").Renderer;
+const VulkanRenderer = @import("renderer/vulkan/VulkanRenderer.zig").VulkanRenderer;
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const gpa = init.arena.allocator();
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+    var window = try Window.init(.{
+        .width = 1280,
+        .height = 720,
+        .title = "FarHorizons 0.0.0",
+    });
+    defer window.deinit();
 
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+    var renderer = try Renderer.init(gpa, &VulkanRenderer.vtable);
+    defer renderer.deinit();
+
+    std.log.info("Entering main loop...", .{});
+
+    while (!window.shouldClose()) {
+        window.pollEvents();
+
+        try renderer.beginFrame();
+        try renderer.render();
+        try renderer.endFrame();
     }
 
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try FarHorizons.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
+    std.log.info("Shutting down...", .{});
 }
 
 test "simple test" {
