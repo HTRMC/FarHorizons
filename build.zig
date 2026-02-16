eaf5b00dff8d0912d22c2f81c71d359aa17fa640
HTRMC <143692436+HTRMC@users.noreply.github.com>
2026-02-16 17:04:13 +0100

    chore: remove template boilerplate and dead code

--- build.zig
+++ build.zig
@@ -45,20 +45,12 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const mod = b.addModule("FarHorizons", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-    });
-
     const exe = b.addExecutable(.{
         .name = "FarHorizons",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
-            .imports = &.{
-                .{ .name = "FarHorizons", .module = mod },
-            },
         }),
     });
 
@@ -75,12 +67,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const mod_tests = b.addTest(.{
-        .root_module = mod,
-    });
-
-    const run_mod_tests = b.addRunArtifact(mod_tests);
-
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
@@ -88,6 +74,5 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
 
     const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_exe_tests.step);
 }
